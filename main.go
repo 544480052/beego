7082e52ea64ee737a1c1b07a03d8b395b0221387
程曦 <544480052@qq.com>
2018-08-16 16:30:52 +0800

    Debug

--- main.go
+++ main.go
@@ -1,15 +1,17 @@
-package main
+package beego
 
 import (
-	"net/http"
+	"net/http"//http请求包
 	"fmt"
 )
 
 func main()  {
+	//将9090端口的请求转派到home函数
 	http.HandleFunc("/",home)
 	http.ListenAndServe(":9090",nil)
 }
 
 func home(w http.ResponseWriter,r *http.Request)  {
+	//输出给客户端信息
 	fmt.Fprintf(w,"hello world!!!!")
 }
