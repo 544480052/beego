3e3d3d1785b2ef65536f2b95b27fe19ef0887289
程曦 <544480052@qq.com>
2018-08-21 21:07:10 +0800

    controller

---
+++ controllers/test.go
@@ -0,0 +1,17 @@
+package controllers
+
+
+import (
+	"github.com/astaxie/beego"
+)
+
+
+type TestController struct {
+	beego.Controller
+}
+
+func (this *TestController) Get()  {
+
+	this.Ctx.WriteString("来耍耍")
+
+}
