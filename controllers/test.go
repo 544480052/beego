9cc2a9b4e4b135f54eb9e694e6317c959da2e54c
544480052 <544480052@qq.com>
2018-08-22 18:08:27 +0800

    Debug

--- controllers/test.go
+++ controllers/test.go
@@ -16,6 +16,7 @@ func (this *TestController) GetAll() {
 
 	u := beego.BConfig.RunMode
 	this.Ctx.WriteString(u)
+	
 	//this.Ctx.WriteString("what")
 	//test := models.GetAllTests()
 	//this.Data["json"] = test
