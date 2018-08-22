b30f202c1ee15dbc07245cbe6e31556394ce66a5
544480052 <544480052@qq.com>
2018-08-22 17:59:10 +0800

    配置

--- main.go
+++ main.go
@@ -7,9 +7,12 @@ import (
 )
 
 func main() {
+	beego.BConfig.RunMode = "test"
+
 	if beego.BConfig.RunMode == "dev" {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
 	}
+
 	beego.Run()
 }
